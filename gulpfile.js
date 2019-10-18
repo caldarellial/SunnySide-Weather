7acfcb1ceaa46883a027b9e577234e2d440214d7
Albert Caldarelli <caldarellial@gmail.com>
2019-10-18 16:49:02 -0400

    Gulpfile start

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
 /* Gulp should compile sass and make client side JS compatible - feel free to add your own tasks! */
+import gulp from 'gulp';
+import gulpSass from 'gulp-sass';
+import nodeSass from 'node-sass';
+
+gulpSass.compiler = nodeSass;
+
+gulp.task('sass', () =>
+  gulp.src('./*.scss')
+  .pipe(gulpSass.on())
+  .pipe(gulp.dest('./css'))
+);
