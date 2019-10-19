6e60551b6774bc0e7b5f90878bab910964c23956
Albert Caldarelli <caldarellial@gmail.com>
2019-10-19 17:27:32 -0400

    style fixes, gulp css modules generation start

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,13 +2,21 @@
 import gulp from 'gulp';
 import gulpSass from 'gulp-sass';
 import nodeSass from 'node-sass';
+import postcss from 'gulp-postcss';
+import modules from 'postcss-modules';
+import concat from 'gulp-concat';
 
 gulpSass.compiler = nodeSass;
 
 function css() {
-  return gulp.src('*.scss')
+  /*return gulp.src('*.scss')
     .pipe(gulpSass.sync())
-    .pipe(gulp.dest('./build/css'));
+    .pipe(gulp.dest('./build/css'));*/
+  return gulp.src(["src/**/*.scss"], { sourcemaps: true, since: gulp.lastRun(css) })
+  .pipe(gulpSass.sync())
+  .pipe(gulp.dest('./build/css'))
+  .pipe(postcss([modules()]))
+  .pipe(concat('styles.css'));
 }
 
 export default gulp.parallel(css)
